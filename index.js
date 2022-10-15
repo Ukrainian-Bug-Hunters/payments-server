faf68171e951ebd54323f311ba9f07065e391702
nazary001 <nazary9911@gmail.com>
2022-10-15 18:35:45 +0300

    feature/add-CRUD

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import payments from './payments.js';
+import { v4 as uuidv4 } from 'uuid';
 
 const server = express();
 
@@ -11,11 +12,41 @@ server.get('/payments', (req, res) => {
     res.status(200).send(payments);
 });
 
+server.get('/payments/:id', (req, res) => {
+    const payment = payments.find( payment => payment.id === req.params.id);
+    if(payment){
+        res.status(200).send(payment);
+    }
+    else{
+        res.status(404).send({message: 'Wrong id'});
+    }
+});
 
+server.post('/payments', (req, res) => {
+    const payment = {...req.body};
+    Object.assign(payment, {
+        id: uuidv4(),
+        status: 'Pending'
+    });
+    payments.push(payment);
+    res.status(200).send(payment);
+});
+
+server.put('/payments/:id', (req, res) => {
+    const payment = payments.find( payment => payment.id === req.params.id);
+    payment.description = req.body.description;
+    res.status(200).send(payment);
+});
+
+server.delete('/payments/:id', (req, res) => {
+    const payment = payments.find( payment => payment.id === req.params.id);
+    const paymentIdx = payments.indexOf(payment);
+    payments.splice(paymentIdx, 1);
+    res.status(200).send(payments);
+});
 
 server.listen(4000, function() {
     console.log(`Server is running on port ${this.address().port}`);
 });
-
 
 
