b44af9f69c39fe83d224cc82b23834ac6a4b7fa5
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2022-10-17 16:19:23 +0300

    added payment validate & change status

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import payments from './payments.js';
+import { v4 as uuidv4} from 'uuid';
+import currencies from './currencies.js'
 
 const server = express();
 
@@ -11,11 +13,43 @@ server.get('/payments', (req, res) => {
     res.status(200).send(payments);
 });
 
+function validatePaymentData (payment) {
+    const currentDate = new Date().toLocaleDateString('fr-CA');
+    if(payment.date >= currentDate &&
+        currencies.includes(payment.currency) &&
+        Number.isFinite(payment.amount) && 
+        Number.isFinite(payment.exchangeRate) &&
+        payment.description &&
+        payment.description !== ''
+    ) {
+        return payment;
+    }
+    else {
+        payment = null;
+        return payment;
+    }
+};
 
+server.post('/payments', (req, res) => {
+    const payment = validatePaymentData({...req.body});
+    if(payment) {
+        Object.assign(payment, {
+            id: uuidv4(),
+            status: 'Pending'
+        });
+
+        payments.push(payment);
+        res.status(200).send(payment);
+
+        setTimeout(function() {
+            payment.status = 'Completed'
+        }, 10000);
+    }
+    else {
+        res.status(404).send('Not Found')
+    }
+});
 
 server.listen(4000, function() {
     console.log(`Server is running on port ${this.address().port}`);
 });
-
-
-
